f63dd11cab3a136167b14e7d80d1c630434d329d
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-03-21 23:10:00 +0330

    Template Literals part is added

---
+++ Template Literals/script.js
@@ -0,0 +1,7 @@
+const name = "Roozbeh";
+const age = 22;
+const text = "Hi I am " + name + " and I am " + age + " years old";
+console.log(text)
+// Template Literals
+const text2 = `Hi I am ${name} and I am ${age} years old`;
+console.log(text2)
